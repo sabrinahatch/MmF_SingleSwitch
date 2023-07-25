952642bb94f8e20aa4541979a22a1db6b2e73aa7
Sabrina <sabrinahatch222@gmail.com>
2023-07-25 13:13:30 -0400

    added a bunch of print statements to debug the code.

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 # CMU REUSE SNAP Lab
 # Summer 2023
 
-# how would I do this for SRPT and try to optimize so that it's an easier transition
-
 import numpy as np
+from itertools import groupby
 
-# create flow class
 class Flow:
     def __init__(self, arrivalTime, size, src, dest, rate, rpt):
         self.arrivalTime = arrivalTime
@@ -33,9 +31,6 @@ class Path:
         self.nextPathArrival = None
         self.nextPathDeparture = None
 
-
-
-
 # fcn to generate job sizes
 def generateJobSize():
     return np.random.exponential(1)
@@ -43,7 +38,6 @@ def generateJobSize():
 # fcn that generates an interarrival time
 def generateInterarrivalTime():
     return np.random.exponential(10 / 8)
-
 
 # fcn to generate all the link objects in the system
 def initializeLinks(k):
@@ -62,7 +56,7 @@ def initializeLinks(k):
         links.extend([src_link, dest_link])
     return links
 
-
+# fcn that makes unique matchings of the links to create every possible path in the system
 def makeMatchings(links):
     src_links = [link for link in links if link.type == "src"]
     dest_links = [link for link in links if link.type == "dest"]
@@ -77,27 +71,34 @@ def makeMatchings(links):
             matchings.append(matching)
 
     return matchings
+
 # function that runs the MmF algorithm on the list of unsatLinks (links that are currently being serviced)
 # function updates link and flow attributes to accurately represent the state of the system in any given point of time
-def maxMinFair(listOfLinks):
-    global unsatLinks, unsatFlows, min_inc, temp, temp2
+def maxMinFair(unsatLinks, unsatFlows, clock):
     # find the min inc for all the flows
-    for i in unsatLinks:
-        for x in i.fol:
-            if x in unsatFlows:
-                temp.append(x)
-        var = (i.cap / len(temp)) if len(temp) > 0 else 0
-        temp2.append(var)
-    min_inc = min(temp2)
+    for link in unsatLinks:
+        print("these are the flows on link attr: " + str(link) + " , " + str(link.fol))
+    # should this be the non zero min? The only time it would be 0 is if the link was saturated or if anothe link was empty
+    min_inc = min([link.cap / len(link.fol) for link in unsatLinks if len(link.fol) > 0])
     print("this is the min inc " +  str(min_inc))
+    print("\n")
+
+
+
     # now update each flows' rate & pdt
     for x in unsatFlows:
         # update each flow's rate and new departure time
-        x.rate = x.rate + min_inc
-        x.pdt = clock + (x.rpt / x.rate)
+        x.rate += min_inc
+        if min_inc == 0:
+            x.pdt = clock + x.rpt
+        else:
+            x.pdt = clock + (x.rpt / x.rate)
+
+
     # calculates the capacity of the links and updates their attributes
     for x in unsatLinks:
-        x.cap = 1 - sum([i.rate for i in x.fol])
+        x.cap = 1 - sum([flow.rate for flow in x.fol])
+
     # iterate over lists of sat links/flows and unsat links/flows and update them accordingly based on the new water
     # filling algo application
 
@@ -122,32 +123,46 @@ def maxMinFair(listOfLinks):
             b += 1
 
 # fcn to handle an arrival event
-def handleArr():
+def handleArr(whichPath):
     # declare all global variables
     global unsatFlows, satFlows, path1, path2, path3, path4, link1, link2, link3, link4, departures, lastEvent, departingJob, nextArrTime, nextDepTime
 
     # create new flow object for new arrival
     size = generateJobSize()
-    flow = Flow(arrivalTime=clock, size=size, src=None, dest=None, rate = 0, rpt = size)
+    # creates the job for the specific path and gives it an initial rate of 0
+    flow = Flow(arrivalTime=clock, size=size, src=whichPath.matching[0], dest=whichPath.matching[1], rate = 0, rpt = size)
+    # update the flows on link attribute for the links in the path the job is traversing
+    whichPath.matching[0].fol.append(flow)
+    whichPath.matching[1].fol.append(flow)
 
-    # implement roundrobin system to assign flows to different paths
+    print("this is the new flow " + str(flow) + " on the links: " + str(flow.src) + " , " + str(flow.dest))
+
+
 
     # append the new flow to the list of unsat flows
     unsatFlows.append(flow)
     # calculate the new minInc for the updated list of flows
-    maxMinFair(unsatLinks)
+    print("This is the list of unsatFlows: " + str(unsatFlows))
+    print("This is the list of unsatLinks: " + str(unsatLinks))
+    maxMinFair(unsatLinks, unsatFlows, clock)
     # update new lastEvent
     lastEvent = clock
-    # assign the next departing job based on the pdt
-    departingJob = min(unsatFlows, key=lambda x: x.pdt)
 
-    # set the next departure and arrival
-    nextArrTime = clock + generateInterarrivalTime()
-    nextDepTime = departingJob.pdt
+
+
+
+    # set the next arrival for the specific path we are on right now
+    whichPath.nextPathArrival = clock + generateInterarrivalTime()
+
+    # set the next departure time
+    nextDepTime = min(unsatFlows, key=lambda x: x.pdt)
+
+    # determine the next arrival based on total list of path arrivals
+    nextArrTime = min(unsatFlows, key=lambda x: x.nextPathArrival)
 
 # fcn to handle a departure event
 def handleDep():
-    global tracker, unsatFlows, satFlows, path1, path2, path3, path4, link1, link2, link3, link4, departures, satLinks, lastEvent, departingJob, nextArrTime, nextDepTime
+    global unsatFlows, satFlows, path1, path2, path3, path4, link1, link2, link3, link4, departures, satLinks, lastEvent, departingJob, nextArrTime, nextDepTime
     # inc the dep counter
     departures += 1
     # set completion time of departing job
@@ -164,7 +179,7 @@ def handleDep():
     if len(unsatLinks) != 0:
         for x in unsatFlows:
             x.rpt = x.rpt - (x.pdt - clock) * x.rate
-        maxMinFair(unsatLinks)
+        maxMinFair(unsatLinks, unsatFlows, clock)
 
         lastEvent = clock
         departingJob = min(unsatFlows, key=lambda x: x.pdt)
@@ -177,12 +192,14 @@ def handleDep():
 
 # sim logic
 seed = 0
-maxDepartures = 10
+maxDepartures = 20
 completionTimes = []
 runs = 5
+
 # input number of ports that you want
 numPorts = 4
 count = 0
+
 for i in range(runs):
     count += 1
     np.random.seed(seed)
@@ -194,50 +211,73 @@ for i in range(runs):
     satFlows = []
     satLinks = []
     unsatFlows = []
+    # list that hold all pending arrivals at any given moment
     allArrivals = []
     allDepartures = []
 
 
-    # Generate links and matchings
-    matchings = makeMatchings(links = initializeLinks(numPorts))
+    # Generate links and matchings and then start out by appending all the newly created
+    # links to unsatLInks list
+    links = initializeLinks(numPorts)
+    unsatLinks = links
+    matchings = makeMatchings(links)
+
+
 
     # Create a path object for each matching
     paths = []
     for index, matching in enumerate(matchings):
         src_link, dest_link = matching
-        name = f"Path_{index + 1}"  # You can use any naming convention here
+        name = f"Path_{index + 1}"
         serviceList = []  # Add flows that belong to this path here
         path = Path(name=name, matching=matching, serviceList=serviceList)
         paths.append(path)
 
 
-    # start the first job off in the correct order for round robin
     departingJob = None
     rate = None
 
-    # add new arrival times for each path list to create pending jobs
-    # add to total list as well to filter out when next job will occur
+    # generate pending jobs for each pathList
     for x in paths:
         x.nextPathArrival = generateInterarrivalTime()
-        allArrivals.append(x.nextPathArrival)
-        print(x.nextPathArrival)
-    print("this is the list of all arrivals: " + str(allArrivals))
-
-
-    # determine the next arrival
-    nextArrTime = min(allArrivals)
-    print("this is the next arrival event: " + str(nextArrTime))
+        print("This is the path: " + str(x.name) + " with next arrival time: " + str(x.nextPathArrival))
+    # determine the first arrival
+    whichPath = min(paths, key = lambda path: path.nextPathArrival)
+    nextArrTime = whichPath.nextPathArrival
     # set first departure time to be inf
     nextDepTime = float("inf")
+
+
+
+
     departures = 0
     clock = 0.0
     lastEvent = clock
+
+    # for x in paths:
+    #     print(x)
+    #     print(x.name)
+    #     print(x.matching)
+    #     print(x.nextPathArrival)
+    #     print("\n")
+
+
+    print("This is the path with the closest arrival time: " + str(whichPath) + " of " + str(whichPath.nextPathArrival))
+    print("\n")
+
+
+
+    # at this point, all the links are unsaturated and there are not
+    # yet any flows to add to the unsatFlows list. Additionally, there are no flows in the
+    # serviceList attr of each of the flows
+
+
     print("********************* This is run: " + str(count) + " *****************************" + "\n")
 
     while departures <= maxDepartures:
         if nextArrTime <= nextDepTime:
             clock = nextArrTime
-            handleArr()
+            handleArr(whichPath)
         else:
             clock = nextDepTime
             handleDep()
